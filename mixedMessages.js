a14ed61091769067913ac8ed847ff63949dee95c
jasonlusk1996 <jasonlusk1996@gmail.com>
2026-03-05 17:30:52 -0600

    First commit with only comments explaining project

---
+++ mixedMessages.js
@@ -0,0 +1,6 @@
+//this is the start of the code for the mixed messages project
+//user runs program
+//store array of class, weapon, and item
+//iterate through each array and randomly select one element from each
+//combine the three elements into a single string and display it to the user
+//logged console will be something like "You are a [class], equipped with a [weapon]. You have a [item] in your inventory."
