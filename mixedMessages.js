cb47b8e786f2e137e0ae65d2c7f25595311dde9e
jasonlusk1996 <jasonlusk1996@gmail.com>
2026-03-06 11:04:04 -0600

    create function to select random value for each key

--- mixedMessages.js
+++ mixedMessages.js
@@ -3,5 +3,10 @@
 //store array of class, weapon, and item
 const loadout = {classes: ['Warrior', 'Mage', 'Rogue','Archer','Monk'], weapons: ['Sword', 'Staff', 'Dagger','Bow','Staff'], items: ['Health Potion', 'Mana Potion', 'Stamina Potion','Poison Potion','Agility Potion']};
 //iterate through each array and randomly select one element from each
-//combine the three elements into a single string and display it to the user
-//logged console will be something like "You are a [class], equipped with a [weapon]. You have a [item] in your inventory."
+function selectLoadout(loadout) {
+    const selectedClass = loadout.classes[Math.floor(Math.random() * loadout.classes.length)];
+    const selectedWeapon = loadout.weapons[Math.floor(Math.random() * loadout.weapons.length)];
+    const selectedItem = loadout.items[Math.floor(Math.random() * loadout.items.length)];
+    //combine the three elements into a single string and display it to the user
+    console.log(`You are a ${selectedClass}, equipped with a ${selectedWeapon}. You have a ${selectedItem} in your inventory.`);
+}
