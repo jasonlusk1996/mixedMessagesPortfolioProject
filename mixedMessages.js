3a9b40863b7010336e9ade5b57617538a56b0092
jasonlusk1996 <jasonlusk1996@gmail.com>
2026-03-06 10:54:52 -0600

    change initial array to dictionary

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,7 +1,7 @@
 //this is the start of the code for the mixed messages project
 //user runs program
 //store array of class, weapon, and item
-const loadout=[['Warrior', 'Mage', 'Rogue','Archer','Monk'], ['Sword', 'Staff', 'Dagger','Bow','Staff'], ['Health Potion', 'Mana Potion', 'Stamina Potion','Poison Potion','Agility Potion']];
+const loadout = {classes: ['Warrior', 'Mage', 'Rogue','Archer','Monk'], weapons: ['Sword', 'Staff', 'Dagger','Bow','Staff'], items: ['Health Potion', 'Mana Potion', 'Stamina Potion','Poison Potion','Agility Potion']};
 //iterate through each array and randomly select one element from each
 //combine the three elements into a single string and display it to the user
 //logged console will be something like "You are a [class], equipped with a [weapon]. You have a [item] in your inventory."
